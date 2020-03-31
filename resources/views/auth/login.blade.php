d2279ee8facb8c26b73b0de5834a411e748ffc5f
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-03-31 21:47:20 +0100

    Addded the summary section

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,17 +10,21 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <div class="alert">
+                            <span> romany@test.com </span><span> admin@test.com </span>
+                        </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +36,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -58,9 +62,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
---
+++ resources/views/livewire/summary-section.blade.php
@@ -0,0 +1,30 @@
+<div class=" ">
+    <h2 class="card-title text-center"> Request summary </h2>
+    <div class="card-body ">
+
+        <table class="table">
+            <thead>
+
+                <h3 class="bg-info p-2"> Airline details </h3>
+
+            </thead>
+            <tr>
+                <th>name:</th>
+                <td>airline name </td>
+                <th>ICAO:</th>
+                <td> icao </td>
+            </tr>
+        </table>
+    </div>
+    <div class="card-body ">
+        <h3 class="bg-info p-2"> Flight details </h3>
+    </div>
+    <div class="card-body ">
+        <h3 class="bg-info p-2"> Route details </h3>
+    </div>
+
+    <div class="card-body ">
+        <h3 class="bg-info p-2"> Schedule details </h3>
+    </div>
+
+</div>
