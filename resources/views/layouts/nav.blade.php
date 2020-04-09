29b5191cd5f6e2c8e7745983f8660ce87d6c44a6
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-04-09 20:53:05 +0100

    Refactor the nav page

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -21,6 +21,16 @@
 
                        </li>
 
+                       <li class="nav-item dropdown border  m-2">
+                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                               {{__('CAA Options')}} <span class="caret"></span>
+                           </a>
+                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                               <a href="{{route('flights.all') }}" class="dropdown-item"> Approved Flights </a>
+                           </div>
+
+                       </li>
+
                        <!-- Agent Options dropdown -->
                        <li class="nav-item dropdown border m-2">
                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
